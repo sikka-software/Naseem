03cd849e8f02634b7e906585c362f9a06dc82d5c
Zakher Masri <zakhermasri@gmail.com>
2026-03-03 06:06:31 +0300

    chore: update logo sizes

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -2,12 +2,21 @@ import type { ReactNode } from "react";
 import { baseOptions, linkItems } from "@/app/layout.config";
 import { source } from "@/lib/source";
 import { DocsLayout, DocsLayoutProps } from "fumadocs-ui/layouts/notebook";
+import NaseemLogo from "@/components/naseem-logo";
 
 const docsOptions: DocsLayoutProps = {
   ...baseOptions,
   i18n: false,
   links: linkItems,
   tree: source.pageTree,
+  nav: {
+    ...baseOptions.nav,
+    title: (
+      <div className="relative flex items-center space-x-2">
+        <NaseemLogo className="size-30 -my-10" />
+      </div>
+    ),
+  },
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
