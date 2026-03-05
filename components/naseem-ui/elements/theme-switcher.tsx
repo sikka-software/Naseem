559845a8ab74a5cc6d338f8ddd00bed45f0db692
Zakher Masri <zakhermasri@gmail.com>
2026-03-05 07:13:38 +0300

    chore: update theme-switcher

--- components/naseem-ui/elements/theme-switcher.tsx
+++ components/naseem-ui/elements/theme-switcher.tsx
@@ -1,40 +1,61 @@
 "use client";
 
-import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
 import { useTranslations, useLocale } from "next-intl";
+
 import { Button } from "./button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../../ui/tooltip";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
   DropdownMenuTrigger,
 } from "./dropdown-menu";
-import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
 export function ThemeSwitcher() {
   const t = useTranslations();
-  const lang = useLocale();
-  const { theme, setTheme, themes, resolvedTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          {resolvedTheme === "dark" ? <Moon /> : <Sun />}
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align={lang === "ar" ? "start" : "end"}>
-        <DropdownMenuRadioGroup
-          value={theme}
-          onValueChange={(e) => setTheme(e)}
+      <Tooltip delayDuration={100}>
+        <TooltipTrigger asChild>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+        </TooltipTrigger>
+        <TooltipContent
+          className="px-2 py-1 text-xs"
+          side="bottom"
+          align="start"
         >
-          {themes?.map((theme) => (
-            <DropdownMenuRadioItem value={theme} key={theme}>
-              {t(theme)}
+          <p>{t("theme")}</p>
+        </TooltipContent>
+      </Tooltip>
+      <DropdownMenuContent align="end">
+        <DropdownMenuGroup>
+          <DropdownMenuRadioGroup
+            value={theme}
+            onValueChange={(e) => setTheme(e)}
+          >
+            <DropdownMenuRadioItem value="light">
+              {t("light")}
             </DropdownMenuRadioItem>
-          ))}
-        </DropdownMenuRadioGroup>
+            <DropdownMenuRadioItem value="dark">
+              {t("dark")}
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="system">
+              {t("system")}
+            </DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
   );
